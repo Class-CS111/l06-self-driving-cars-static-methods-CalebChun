74af1c6a9972a0801a1cd67f4d08352325c1c6a2
CalebChun <1024656@oside.us>
2025-04-17 12:33:06 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,33 @@
 // When you are ready to have your work checked, change the value below from N to Y
-// ReadyForSubmission=N
-// YOUR NAME:
+// ReadyForSubmission=Y
+// YOUR NAME:Caleb
 // COLLABORATORS:
 // DATE:  
 
 public class Main
 {
-	public static void main(String[] args)
-	{
-		System.out.println("My thoughts on the article are:");
-		// Part 2: Invoking/calling method
-		
-	}
-	
-	// Part 1: Creating method definition
-	
+    public static void main(String[] args)
+    {
+        //part2
+        System.out.println("My thoughts on the article are: ");
+        opinionOne();
+        opinionTwo();
+        opinionThree();
+    }
+    
+    //part1
+    public static void opinionOne()
+    {
+        System.out.println("Firstly, the car should prioritize the safety of the pedestrians\nsince they are more likely to get injured");
+    }
+    
+    public static void opinionTwo()
+    {
+        System.out.println("Next, the car should be able to recognize it's surroundings\nand if possible the heights of the pedestrians to prioritize\nthe young or old");
+    }
+    
+    public static void opinionThree()
+    {
+        System.out.println("For my last statement, the responsibility of the\nprioritization should be on the car company, because they are\nthe ones that are realeasing the cars for sale");
+    }
 }
